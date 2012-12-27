df1ed787b17283e5351fd151d7ede06c2be2fb9c
Phillip Lackey <philliplackey@aimac.corp.athlon.com>
2012-12-27 17:04:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AthlonAdmin::Application.config.secret_token = '0e2c073aff7ce8f1c70152a15775743fbbf9759ec13d16458c3b3a8ac7dee86885419d9d831cd88fa4a8dbd6422c4a2be533d17caab50726f93247e008ef489e'
